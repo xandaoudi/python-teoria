488078b139b2d80fd606cb95a00b17bafe398e81
xandaoUdi <alexandrecostaoliv88@gmail.com>
2020-04-15 11:21:01 -0300

    theoretical classes number 8

---
+++ aula04/desafio3.py
@@ -0,0 +1,4 @@
+n1 = int(input('Primeiro número'))
+n2 = int(input('Segundo número'))
+
+print(n1+n2)
---
+++ aula04/ex01Aula4.py
@@ -0,0 +1,10 @@
+nome = 'Guanabara'
+idade = 25
+peso = 74.8
+print(nome, idade, peso)
+'''
+print(nome + idade + peso)
+'''
+
+nome = input('Qual é o seu nome?')
+print(nome)
